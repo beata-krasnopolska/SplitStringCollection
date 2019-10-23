a518b4d07a5f864734855caf1c8c053ed06d286f
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-10-23 19:35:37 +0200

    Add additional check by LINQ method

--- SplitCollectionString/Program.cs
+++ SplitCollectionString/Program.cs
@@ -13,11 +13,21 @@ namespace SplitCollectionString
             List<string> cities = new List<string>() { "Warsaw", "Rome", "Paris", "Lisbon", "New York", "London", "Berlin", "Moscow", "Oslo", "Madrid", "Barcelona", "Hong Kong" };
 
             var groupedCities = from city in Enumerable.Range(0, cities.Count)
+                                orderby city
                                 group cities[city] by city / 4;
 
+            var groupsLambda = Enumerable.Range(0, cities.Count).GroupBy(x => x / 4, y => cities[y]);
+
+            Console.WriteLine("First check by LINQ Query:");
             foreach (var city in groupedCities)
             {
                 Console.WriteLine(string.Join(";  ", city.ToArray()));
+            }
+            Console.WriteLine();
+            Console.WriteLine("Second check by LINQ method:");
+            foreach(var city in groupsLambda)
+            {
+                Console.WriteLine(string.Join("; ", city.ToArray()));
             }
             Console.ReadKey();
         }
